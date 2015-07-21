f779e1176850c65676f256aadec04e93a0913d75
Iliyan Betovski <ibetovski@gmail.com>
2015-07-21 20:29:44 +0300

    Switches to jest v.0.5 because of io.js usage.

---
+++ __tests__/func.spec.js
@@ -0,0 +1,24 @@
+// jest.dontMock('../app/actions/ListActions.js');
+// jest.dontMock('../app/stores/ListStore.js');
+// jest.dontMock('../app/components/List.js');
+
+describe('Functional List', function() {
+  // var React;
+  // var TestUtils;
+  // var Component;
+  // var Action;
+  // var Store;
+
+  beforeEach(function() {
+    // React = require('react/addons');
+    // TestUtils = React.addons.TestUtils;
+
+    // Actions = require('../app/components/List.js');
+    // Actions = require('../app/actions/ListActions.js');
+    // Store = require('../app/stores/ListStore.js');
+  });
+
+  it('should list the items from the store', function() {
+    expect(1).toEqual(1);
+  });
+});
